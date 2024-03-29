f4e84be85e8bbde1969dfb3e1156b38ca3f70325
Maricarmen <nemracira1718@gmail.com>
2024-03-29 18:08:19 -0500

    correciones

--- frontend/src/components/AddPlacePopup.js
+++ frontend/src/components/AddPlacePopup.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import PopupWithForm from './PopupWithForm'
+
 
 function AddPlacePopup({isOpen, onClose, onAddPlace}) {
     const [placeName, setPlaceName] = useState("");
     const [placeLink, setPlaceLink] = useState("");
+
+    useEffect(() => {
+      setPlaceName("");
+      setPlaceLink("")
+  }, [isOpen]);
 
     function handleSubmit(evt){
         evt.preventDefault()
@@ -18,9 +24,9 @@ function AddPlacePopup({isOpen, onClose, onAddPlace}) {
     }
   return (
     <PopupWithForm title="Nuevo lugar" name="place" isOpen={isOpen} onClose={onClose} onSubmit={handleSubmit} >
-    <input type="text" name="title" id="title"  className="popup__text" placeholder="Título" minLength="2" maxLength="40" required onChange={onNameChange} />
+    <input type="text" name="title" id="title"  className="popup__text" placeholder="Título" minLength="2" maxLength="40" required onChange={onNameChange} value={placeName}/>
     <span className="popup__input-error name-error" id="name-error"></span>
-    <input type="url" name="src" id="link"  className="popup__text" placeholder="Enlace a la imagen" minLength="2" maxLength="200" required onChange={onLinkChange} />
+    <input type="url" name="src" id="link"  className="popup__text" placeholder="Enlace a la imagen" minLength="2" maxLength="200" required onChange={onLinkChange} value={placeLink} />
     <span className="popup__input-error about-me-error" id="link-error"></span>
     </PopupWithForm>
   )
--- frontend/src/components/PopupWithForm.js
+++ frontend/src/components/PopupWithForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 function PopupWithForm({title, name , children,isOpen,onClose, onSubmit}) {
+
   return (
     <form className={`popup popup_type_${name} ${isOpen ? 'popup__opened':''}`}  id="popup-profile" onSubmit={(evt)=>{
       onSubmit(evt);
